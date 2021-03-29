a76a419ca81dfe96406970c0145030cc06ee0ae0
chambior <remi@potirouilles.com>
2021-03-30 01:35:15 +0200

    les dieux sont tout beaux

--- godboard.py
+++ godboard.py
@@ -11,11 +11,13 @@ def find(e,a):
 class God:
     def __init__(self, screen, name, pos):
         self.name = name
+        self.screen = screen
         self.lastplayer = 0
         self.lastvalue = 0
         self.currentplayer = 0
         self.currentvalue = 0
         self.pos = pos
+        self.rect = pygame.Rect(260, 250+95*pos, 270, 90)
 
     def update(self, pos, auctions):
         self.pos = pos
@@ -24,9 +26,12 @@ class God:
             self.lastplayer = self.currentplayer
             self.currentplayer = auctions[0]
             self.currentvalue = auctions[1]
+        self.rect = pygame.Rect(260, 250+95*pos, 270, 90)
 
     def draw(self):
-        pass
+        #pygame.draw.rect(self.screen, pygame.Color("blue"), self.rect, 2)
+        print("Place : {} Nom : {}".format(self.pos, self.name))
+        self.screen.blit(textures.getImage('gods', self.name), [self.rect.x, self.rect.y])
 
 class GodBoard:
     def __init__(self, screen, gods, auctions):
@@ -34,20 +39,23 @@ class GodBoard:
         self.gods = []
         #             Place d'ares , Place de popo, Place de Athe, Place de zeux
         self.order = [find(1, gods), find(2, gods), find(3, gods), find(4, gods)]
-        for god in gods:
-            self.gods.append(God(screen, 'ares'    , self.order[0]))
-            self.gods.append(God(screen, 'poseidon', self.order[1]))
-            self.gods.append(God(screen, 'athena'  , self.order[2]))
-            self.gods.append(God(screen, 'zeus'    , self.order[3]))
+
+        self.gods.append(God(screen, 'ares'    , self.order[0]))
+        self.gods.append(God(screen, 'poseidon', self.order[1]))
+        self.gods.append(God(screen, 'athena'  , self.order[2]))
+        self.gods.append(God(screen, 'zeus'    , self.order[3]))
+        self.gods.append(God(screen, 'appolon', 4))
         self.update(gods, auctions)
-        self.rect = pygame.Rect(255, 245, 280, 469)
+        #self.rect = pygame.Rect(255, 245, 280, 469)
 
     def update(self, gods, auctions):
         #ranger les dieux (self.order)
         for i in range(4):
             self.gods[i].update(self.order[i], auctions[i])
+        self.gods[4].update(4, (0,0,0))
 
     def draw(self):
         for god in self.gods:
             god.draw()
-        pygame.draw.rect(self.screen, pygame.Color("white"), self.rect, 2)
+        #pygame.draw.rect(self.screen, pygame.Color("white"), self.rect, 2)
+        self.screen.blit(textures.getImage('overlays','gods'), [255,5])
