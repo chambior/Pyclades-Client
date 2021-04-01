d906903e26fcff91371dc9f49159be28d3fc7d42
chambior <remi@potirouilles.com>
2021-04-01 03:32:52 +0200

    début affichage créatures

--- godboard.py
+++ godboard.py
@@ -46,6 +46,20 @@ class God:
         if(self.currentplayer):
             #print("Params de getimage :\n{}\n{}".format('gods', 'mark_{}'.format(self.currentplayer)))
             self.screen.blit(textures.getImage('gods', 'mark_{}'.format(self.currentplayer)),[self.rect.x+8+21*self.currentplayer, self.rect.y+60])
+
+
+class Creature_board:
+    def __init__(self, surface, creatures):
+        self.surface = surface
+        self.creatures = creatures
+
+        self.rect = pygame.Rect(255, 6, 280, 242)
+    def update(self, creatures):
+        self.creatures = creatures
+    def draw(self):
+        pygame.draw.rect(self.surface, pygame.Color("Cyan"), self.rect, 2)
+
+
 class GodBoard:
     def __init__(self, screen, gods, auctions):
         self.screen = screen
@@ -58,17 +72,22 @@ class GodBoard:
         self.gods.append(God(screen, 'athena'  , self.order[2]))
         self.gods.append(God(screen, 'zeus'    , self.order[3]))
         self.gods.append(God(screen, 'appolon', 4))
+
+        self.creature = Creature_board(screen, [])
+
         self.update(gods, auctions)
         #self.rect = pygame.Rect(255, 245, 280, 469)
 
     def update(self, gods, auctions):
+        self.creature.update([])
         self.order = [find(1, gods), find(2, gods), find(3, gods), find(4, gods)]
         for i in range(4):
             self.gods[i].update(self.order[i], auctions[i])
         self.gods[4].update(4, (0,0,0))
 
     def draw(self):
+        self.creature.draw()
         for god in self.gods:
             god.draw()
         #pygame.draw.rect(self.screen, pygame.Color("white"), self.rect, 2)
-        self.screen.blit(textures.getImage('overlays','gods'), [255,5])
+        #self.screen.blit(textures.getImage('overlays','gods'), [255,5])
